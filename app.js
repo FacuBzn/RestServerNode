608c2b1972c6dc24304ca49cb28872254b2e761d
Farsante <juanfacundobazanalvarez@gmail.com>
2021-10-22 16:24:53 -0300

    se actualizan folders del proyecto

--- app.js
+++ app.js
@@ -3,6 +3,11 @@ const Server = require('./models/server');
 
 
 const server = new Server();
-server.listen();
+/* server.listen(); */
+
+/* app.listen(8080) */
+server.listen(port, () => {
+    console.log(`Example app listening at http://localhost:${port}`)
+})
 
 
